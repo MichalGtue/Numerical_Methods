d6680df1ba2c05616eae3c8af3a0adb7c30bf9ca
MichalGTue <m.j.gajda@student.tue.nl>
2023-12-04 09:41:56 +0100

    task 2 fix

--- Assignment 2/Task 2.py
+++ Assignment 2/Task 2.py
@@ -182,7 +182,36 @@ def gaussseidel_vec(A, b, tol=1e-2):
     
     return x, it_gaussseidel
 
-  
+
+#task 2 matrix
+#Define the values of the Q's (given in km^-3 / yr)
+Q_SH = 72
+Q_MH = 38
+Q_HE = 160
+Q_EO = 185
+Q_OO = 215
+
+#Define values for the different PCB source S_in's (given in kg/yr)
+
+S_in   = 180
+S_in_M = 810
+S_in_H = 630
+S_in_E = 2750
+S_in_O = 3820
+
+#Volumes (km^3)
+
+V_S = 12000
+V_M = 4900
+V_H = 3500
+V_E = 480
+V_O = 1640
+
+
+
+#Generating our coefficient matrix
+M = np.array([[Q_SH,0,0,0,0], [0,Q_MH,0,0,0], [-Q_SH, -Q_MH, Q_HE, 0, 0], [0,0,-Q_HE, Q_EO , 0], [0,0,0,-Q_EO,Q_OO]])
+#print(M) #Uncomment to see the coefficient matrix
 
 
 #Solution Vector
