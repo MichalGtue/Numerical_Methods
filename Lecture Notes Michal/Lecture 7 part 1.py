8536dd393123e9c949faa75e42072eede8386ee4
MichalGTue <m.j.gajda@student.tue.nl>
2023-12-05 15:38:02 +0100

    lecture  7

--- Lecture Notes Michal/Lecture 7 part 1.py
+++ Lecture Notes Michal/Lecture 7 part 1.py
@@ -1,4 +1,4 @@
-
+import numpy as np
 
 #Newton method
 
@@ -21,8 +21,66 @@ def newton_method(func, funcprime, x1, nguess=100, tol_x=1e-6, tol_f=1e-6):
         print(counter)
     return x1
 
-def newton_class(func,grad,x,tol_x,tol_f):
-    
+def newton_class(func,x,tol_x,tol_f):
+    '''Newton-raphson, no input of derivative info'''
+    itmax = 100
+    converged = False
+    h=1e-8
+    it = 0
+    f = func(x)
+    while(not converged) and (it<itmax):
+        it +=1
+        g = (func(x+h) - f)/h
+        dx = -f/g
+        x +=dx
+        f = func(x)
+        converged = (abs(dx) <= tol_x) and (abs(f) <= tol_f)
+        print(it)
+    if converged:
+        print(f" Newton root found in {it}")
     return x
 
-print(newton_method(fnc,fncprime, 100))
+#print(newton_class(fnc, 5, 1e-15, 1e-15))
+
+
+# Newton raphson for multidimensional
+
+
+
+def func(x):
+    n  = x.size
+    fnc = np.zeros(n)
+    fnc[0] = x[0]**2 + x[1]**2 - 4
+    fnc[1] = x[0]**2 - x[1] + 1
+    return fnc
+
+def jac(x):
+    n = x.size
+    jac = np.zeros((n,n))
+    jac[0,0] = 2*x[0]
+    jac[0,1] = 2*x[1]
+    jac[1,0] = 2*x[0]
+    jac[0,1] = -1
+    return jac
+
+
+def newton_class_multivariable(func,x,tol_x,tol_f):
+    '''Newton-raphson, no input of derivative info. Multivariable'''
+    itmax = 100
+    converged = False
+    x = np.asarray(x)
+    it = 0
+    f = func(x)
+    while(not converged) and (it<itmax):
+        it +=1
+        g = jac(x)
+        dx = np.linalg.solve(g, -f)
+        x = x + dx
+        f = func(x)
+        converged = (np.max(abs(dx)) <= tol_x) and (np.max(abs(f)) <= tol_f)
+        print(f"it:{it:2}, x[0]: {x[0]:18.16f}, x[1]: {x[1]:18.16f}, f[0]:{f[0]:23.15e}, f[1]:{f[1]:23.15e}")
+    if converged:
+        print(f" Newton root found in {it}")
+    return x
+
+newton_class_multivariable(fnc, [1,2], 1e-16, 1e-16)
