b9c0385436062f9e99571903ce8702bd569166d9
MichalGTue <m.j.gajda@student.tue.nl>
2023-12-18 11:58:23 +0100

    message

--- Lecture Notes Michal/Lecture 11 part 4.py
+++ Lecture Notes Michal/Lecture 11 part 4.py
@@ -1,30 +1,30 @@
 ## Solving PDEs 
 import numpy as np
 import matplotlib.pyplot as plt
-Nt = 40000
-Nx = 100
-D = 1e-8
-cL = 1
-cR = 0
-t_end = 5000
-x_end = 5e-3
 
-kR = 0.01
 
-def reaction(c):
-    return -kR*c**2
+
+Nt = 100000
+Nx = 1000
+u = 0.001
+c_in = 1
+t_end = 100
+x_end = 0.1
 
 dt = t_end/Nt
 dx = x_end/Nx
-Fo = (D*dt)/(dx**2)
+Co = u*dt/dx
 
 
-x = np.linspace(0,x_end, Nx+1)
+
+
+x=np.linspace(0,x_end, Nx+1)
 c = np.zeros(Nx+1)
+c[0] = c_in
+t= 0
 
-c[0] = cL
-c[Nx] = cR
-t = 0
+
+
 
 plt.ion()
 figure, ax = plt.subplots(figsize = (10, 5))
@@ -34,20 +34,20 @@ plt.title(f"Time: {t:5.3f} s", fontsize = 16)
 plt.xlabel('Axial position', fontsize = 14)
 plt.ylabel('Concentration', fontsize=14)
 plt.xlim(0, x_end)
-plt.ylim(0, max(cL, cR))
 plt.grid
 iplot = 0
 
-for n in range(Nt):
-    t += dt
-    c_old = np.copy(c) ## doing c_old = c then modification of c_old will also modify c
-    c[0] = cL
 
-    rxn = reaction(c_old)
-    for i in range(1, Nx):
-        c[i] = Fo *c_old[i-1] + (1-2*Fo)*c_old[i] + Fo*c_old[i+1] + rxn[i]*dt
+for n in range(Nt):
+    t +=dt
+    c_old = np.copy(c)
+
+    c[0] = c_in
+    for i in range(1,Nx):
+        c[i] = c_old[i] - Co*0.5*(c_old[i]-c_old[i-1])
     c[Nx] = c[Nx-1]
 
+    
     iplot += 1
     if (iplot % 100 == 0):
         plt.title(f"Time = {t:5.3f} s")
@@ -57,7 +57,8 @@ for n in range(Nt):
         iplot = 0
 
 
-plt.pause()
+#plt.pause(0.0001)
 plt.show()
+
 
 
