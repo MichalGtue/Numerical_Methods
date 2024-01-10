da501ae4da0b22ff3a0ad1ac9024ec0f31743dcc
John Doe <johndoe@email.com>
2024-01-10 15:31:22 +0100

    task 4 start

--- Assignment 4/Task4 a.py
+++ Assignment 4/Task4 a.py
@@ -3,17 +3,6 @@ import matplotlib.pyplot as plt
 import scipy.sparse as sps
 import scipy.sparse.linalg as spss
 
-def simpson_rule(xlist, ylist):
-    '''Calculates the area of a function using the Simpson rule. \n
-    Takes 3 arguments, first the function, then the lower integration bound, and finally the upper integration bound. \n
-    Optional 4th argument to specify number of intervals (default set to 20)'''
-    sum = 0
-    dx = (xlist[1]-xlist[0])
-    for i in range(len(ylist)):
-        if i>0 and i<len(ylist)-1:
-            sum += (dx/6) * (ylist[i-1]+4*ylist[i]+ylist[i+1])
-    return sum
-
 
 
 # Getting all the variables
@@ -21,14 +10,16 @@ Nx = 100 ## Grid points
 Nt = 10000 # Time steps
 diam = 0.04 # Diameter
 phiv = 6.3e-4 #volumetric flow rate
+phivnit = 11.2e-4 #volumetric flow rate nitrogen
 D = 1e-3
 cL = 1 # Inlet conc
 cR = 0 
-t_end = 4 # 4 sec
+t_end = 10 # 4 sec
 x_end = 1 # Length
 kR = 1.2 
 
 u = phiv / (np.pi*(diam/2)**2) 
+u_nit = phivnit / (np.pi*(diam/2)**2) 
 
 dt = t_end / Nt
 dx  = x_end / Nx
@@ -40,8 +31,10 @@ Fo = D * dt / dx**2
 
 # Courant number
 Co = u* dt /dx
+Co_nit = u_nit*dt/dx
 #print(Co)
-#Uncomment to see that Co is less than 1
+#print(Co_nit)
+#Uncomment to see that Co and Co_nit is less than 1
 
 x = np.linspace(0,x_end, Nx+1)
 c = np.zeros(Nx+1)
@@ -75,6 +68,20 @@ for i in range(1, Nx):
     if dx*i < 0.1 or dx*i>0.9:
         A[i, i] = A[i,i] - kR*dt ## Taking away the reaction term  
 
+A_new = sps.diags([-(Co_nit+Fo), (1+Co_nit+2*Fo+kR*dt), -Fo], [-1, 0, 1], shape=(Nx+1, Nx+1))
+A_new = sps.csr_matrix(A_new)
+A_new[0,0]=1
+A_new[0,1] = 0
+A_new[Nx, Nx] = 1
+A_new[Nx, Nx-1] = -1
+for i in range(1, Nx):
+    if dx*i < 0.1 or dx*i>0.9:
+        A_new[i, i] = A_new[i,i] - kR*dt
+dense_A = A_new.toarray()
+mirrored_A_not_sparse = dense_A[::-1, :] ## Flipping the matrix along the 'x' axis
+mirrored_sparse_A = sps.csr_matrix(mirrored_A_not_sparse)
+
+
 for n in range(Nt):
     t += dt
     c_old = np.copy(c) ## doing c_old = c then modification of c_old will also modify c
@@ -82,22 +89,14 @@ for n in range(Nt):
 
     b = c_old
     b[Nx] = 0
-    c = spss.spsolve(A, b)
-
+    if n*dt<5:
+        c = spss.spsolve(A, b)
+    else:
+        c = spss.spsolve(mirrored_sparse_A,b)
     iplot += 1
-    if (iplot % 100 == 0):
+    if (iplot % 10 == 0):
         plt.title(f"Time = {t:5.3f} s")
         line[0].set_ydata(c)
         figure.canvas.draw()
         figure.canvas.flush_events()
         iplot = 0
-
-
-plt.show()
-
-sum_conc = simpson_rule(x,c) #Conc. given in mol/m^3
-#To get sum of moles we need to multiply by the total volume
-
-sum_moles = sum_conc * np.pi * (diam/2)**2 * x_end
-
-print(f'The total amount of moles lost is {sum_moles}')
